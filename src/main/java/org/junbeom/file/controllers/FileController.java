6e39a5aa991f527857396cd1f5f72aac676fd291
beom33 <byby5546@naver.com>
2024-08-02 12:13:28 +0900

    파일 공통 모듈

--- src/main/java/org/junbeom/file/controllers/FileController.java
+++ src/main/java/org/junbeom/file/controllers/FileController.java
@@ -1,21 +1,38 @@
 package org.junbeom.file.controllers;
 
-import org.springframework.util.StringUtils;
+import lombok.RequiredArgsConstructor;
+import org.junbeom.file.entities.FileInfo;
+import org.junbeom.file.services.FileUploadService;
+import org.junbeom.global.exceptions.RestExceptionProcessor;
+import org.junbeom.global.rests.JSONData;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
 @RequestMapping("/file")
-public class FileController {
+@RequiredArgsConstructor
+public class FileController implements RestExceptionProcessor {
+
+    private final FileUploadService uploadService;
 
         @PostMapping("/upload")
-        public void upload(@RequestPart("file")MultipartFile[] files,
-                           @RequestParam(name="gid", required = false ) String gid,
-                           @RequestParam(name="location", required = false)  String location) {
+        public ResponseEntity <JSONData> upload(@RequestPart("file")MultipartFile[] files,
+                                              @RequestParam(name="gid", required = false )
+                                              String gid,
+                                              @RequestParam(name="location", required = false)
+                                                        String location) {
 
-            gid = StringUtils.hasText(gid) ? gid : UUID.randomUUID().toString();
+            List<FileInfo> items = uploadService.upload(files, gid, location);
+            HttpStatus status = HttpStatus.CREATED;
+            JSONData data = new JSONData(items);
+            data.setStatus(status);
+
+            return ResponseEntity.status(status).body(data);
 
         }
 
