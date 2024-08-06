c435180aca66c4f2e0f772389a498bb2991376cc
beom33 <byby5546@naver.com>
2024-08-06 16:08:44 +0900

    공공데이터 API

--- src/main/java/org/junbeom/tour/entities/TourPlace.java
+++ src/main/java/org/junbeom/tour/entities/TourPlace.java
@@ -18,33 +18,37 @@ public class TourPlace extends BaseEntity {
     private Long contentId;
     private Long contentTypeId;
 
-    @Column(length = 30)
+    @Column(length=30)
     private String category1;
 
-    @Column(length = 30)
+    @Column(length=30)
     private String category2;
 
-    @Column(length = 30)
+    @Column(length=30)
     private String category3;
 
-    @Column(length = 100)
+    @Column(length=100)
     private String title;
 
-    @Column(length = 30)
+    @Column(length=30)
     private String tel;
 
-    @Column(length = 30)
+    @Column(length=150)
     private String address;
+
+    @Column(length=20)
     private String areaCode;
+
     private boolean bookTour;
     private double distance;
+
     private String firstImage;
     private String firstImage2;
+
+    @Column(length=30)
     private String cpyrhtDivCd;
     private double latitude; // mapy
     private double longitude; // mapx
     private int mapLevel;
-    private int sigunguCode;
-
-
+    private int sigugunCode;
 }
