c421d7b2cb7e8eb90574b974f6e2faeab102d7fd
beom33 <byby5546@naver.com>
2024-08-06 14:09:59 +0900

    파일 공통 모듈

--- src/main/java/org/junbeom/main/controllers/MainController.java
+++ src/main/java/org/junbeom/main/controllers/MainController.java
@@ -2,17 +2,21 @@ package org.junbeom.main.controllers;
 
 import org.junbeom.global.exceptions.ExceptionProcessor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/")
 public class MainController implements ExceptionProcessor {
 
     @GetMapping
-      public String index() {
+      public String index(Model model) {
 
-
+        model.addAttribute("addCommonScript",  List.of("fileManger"));
+        model.addAttribute("addScript", List.of("test/form"));
 
          return "front/main/index"; // 템플릿 경로
      }
--- src/main/resources/static/common/js/fileManager.js
+++ src/main/resources/static/common/js/fileManager.js
@@ -8,7 +8,7 @@ const fileManager = {
     * 파일 업로드
     *
     */
-    upload() {
+    upload(files, gid, location) {
 
     },
     /**
@@ -26,3 +26,35 @@ const fileManager = {
 
     }
 };
+
+
+window.addEventListener("DOMContentLoaded", function() {
+    // 파일 업로드 버튼 이벤트 처리 S
+    const fileUploads = document.getElementsByClassName("fileUploads");
+    const fileEl = document.createElement("input");
+    fileEl.type = 'file';
+    fileEl.multiple = true;
+
+    for (const el of fileUploads) {
+        el.addEventListener("click", function() {
+            fileEl.value = "";
+            delete fileEl.gid;
+            delete fileEl.location;
+
+            const dataset = this.dataset;
+            fileEl.gid = dataset.gid;
+            if (dataset.location) fileEl.location = dataset.location;
+
+            fileEl.click();
+
+        });
+    }
+    // 파일 업로드 버튼 이벤트 처리 E
+
+    // 파일 업로드 처리
+    fileEl.addEventListener("change", function(e) {
+        const files = e.target.files;
+        fileManager.upload(files, fileEl.gid, fileEl.location);
+    });
+
+});
